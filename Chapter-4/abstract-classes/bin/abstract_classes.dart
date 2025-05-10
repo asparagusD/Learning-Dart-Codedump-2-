4c90dcba23929cc5b5255da0e59e46e73f77b158
Naufel Aurnob <nufel1130@gmail.com>
2025-05-11 05:16:10 +0600

    example code

---
+++ Chapter-4/abstract-classes/bin/abstract_classes.dart
@@ -0,0 +1,35 @@
+void main() {
+  Animal platypus = Platypus();
+  print(platypus.isAlive);
+  platypus.eat();
+  platypus.move();
+  // platypus.layEggs();
+  print(platypus);
+}
+
+abstract class Animal {
+  bool isAlive = true;
+  void eat();
+  void move();
+
+  @override
+  String toString() {
+    return "I'm a $runtimeType";
+  }
+}
+
+class Platypus extends Animal {
+  @override
+  void eat() {
+    print('Munch munch');
+  }
+
+  @override
+  void move() {
+    print('Glide glide');
+  }
+
+  void layEggs() {
+    print('Plop plop');
+  }
+}
